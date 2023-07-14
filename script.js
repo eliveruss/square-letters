ef5da433db60324288aceef46ea1426606cd9689
Olek <aleksander.galdyn@o2.pl>
2023-07-14 19:46:03 +0200

    initial commit, structure and styles

---
+++ script.js
@@ -0,0 +1,9 @@
+"use strict";
+
+// MAIN VARIABLES //////////////////////////////////////////////////////////
+
+const input = document.querySelector(".squares__menu__input");
+const btn = document.querySelector(".squares__menu__btn");
+const square = document.querySelectorAll(".squares__square");
+
+////////////////////////////////////////////////////////////////////////////
